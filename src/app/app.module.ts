d298ee7b585c50658045b89fba2d3e77b4a24db3
Ed Opare-Aryee <ed@thisissoon.com>
2017-12-03 02:29:32 +0000

    set up up angular service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { GameService } from './game';
 import { BoardComponent } from './board';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
   ],
   providers: [
     GameService

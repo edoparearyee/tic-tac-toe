b30eb8ce91b77d6835dd517e0e6a5278d37039cb
Ed Opare-Aryee <ed@thisissoon.com>
2017-12-09 21:57:28 +0000

    add e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -5,10 +5,150 @@ describe('tic-tac-toe App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should place an "X" in the tile', () => {
+    page.clickTile(0, 1);
+    page.getTileText(0, 1).then((result) => {
+      expect(result).toEqual('X');
+    });
+
+    page.getCurrentPlayerText().then(result => {
+      expect(result).toEqual('Turn: Player 2');
+    });
+  });
+
+  it('should place an "O" in the tile', () => {
+    page.clickTile(0, 1);
+    page.clickTile(1, 2);
+    page.getTileText(1, 2).then((result) => {
+      expect(result).toEqual('O');
+    });
+    page.getCurrentPlayerText().then(result => {
+      expect(result).toEqual('Turn: Player 1');
+    });
+  });
+
+  it('should NOT replace tile and not change turns', () => {
+    page.clickTile(0, 1);
+    page.getTileText(0, 1).then((result) => {
+      expect(result).toEqual('X');
+    });
+    page.clickTile(0, 1);
+    page.getTileText(0, 1).then((result) => {
+      expect(result).toEqual('X');
+    });
+
+    page.getCurrentPlayerText().then(result => {
+      expect(result).toEqual('Turn: Player 2');
+    });
+  });
+
+  it('should declare player 1 the winner', () => {
+    page.clickTile(0, 0);
+    page.clickTile(1, 0);
+    page.clickTile(0, 1);
+    page.clickTile(1, 1);
+    page.clickTile(0, 2);
+
+    page.getWinnerText().then(result => {
+      expect(result).toEqual('Player 1 has won!');
+    });
+  });
+
+  it('should declare player 2 the winner', () => {
+    page.clickTile(0, 0);
+    page.clickTile(0, 1);
+    page.clickTile(0, 2);
+    page.clickTile(1, 1);
+    page.clickTile(2, 0);
+    page.clickTile(2, 1);
+
+    page.getWinnerText().then(result => {
+      expect(result).toEqual('Player 2 has won!');
+    });
+  });
+
+  it('should declare a draw', () => {
+    page.clickTile(0, 0);
+    page.clickTile(1, 1);
+    page.clickTile(0, 2);
+    page.clickTile(0, 1);
+    page.clickTile(2, 1);
+    page.clickTile(1, 0);
+    page.clickTile(1, 2);
+    page.clickTile(2, 2);
+    page.clickTile(2, 0);
+
+    page.getDrawText().then(result => {
+      expect(result).toEqual('Match is a draw');
+    });
+  });
+
+  it('should win if all letters in row is the same', () => {
+    page.clickTile(0, 0);
+    page.clickTile(1, 0);
+    page.clickTile(0, 1);
+    page.clickTile(1, 1);
+    page.clickTile(0, 2);
+
+    page.getWinnerText().then(result => {
+      expect(result).toEqual('Player 1 has won!');
+    });
+  });
+
+  it('should win if all letters in column is the same', () => {
+    page.clickTile(0, 0);
+    page.clickTile(0, 1);
+    page.clickTile(1, 0);
+    page.clickTile(1, 1);
+    page.clickTile(2, 0);
+
+    page.getWinnerText().then(result => {
+      expect(result).toEqual('Player 1 has won!');
+    });
+  });
+
+  it('should win if all letters in diagonal line the same', () => {
+    page.clickTile(0, 0);
+    page.clickTile(0, 1);
+    page.clickTile(1, 1);
+    page.clickTile(2, 1);
+    page.clickTile(2, 2);
+
+    page.getWinnerText().then(result => {
+      expect(result).toEqual('Player 1 has won!');
+    });
+
+    page.reset();
+
+    page.clickTile(0, 2);
+    page.clickTile(0, 1);
+    page.clickTile(1, 1);
+    page.clickTile(2, 1);
+    page.clickTile(2, 0);
+
+    page.getWinnerText().then(result => {
+      expect(result).toEqual('Player 1 has won!');
+    });
+  });
+
+  it('should reset game', () => {
+    page.clickTile(0, 0);
+    page.clickTile(0, 1);
+    page.clickTile(1, 1);
+    page.clickTile(2, 1);
+    page.clickTile(2, 2);
+
+    page.getWinnerText().then(result => {
+      expect(result).toEqual('Player 1 has won!');
+    });
+
+    page.reset();
+
+    page.getCurrentPlayerText().then(result => {
+      expect(result).toEqual('Turn: Player 1');
+    });
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,27 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  clickTile(row: number, col: number) {
+    return element(by.css(`app-board .tile-${row}-${col}`)).click();
+  }
+
+  getTileText(row: number, col: number) {
+    return element(by.css(`app-board .tile-${row}-${col}`)).getText();
+  }
+
+  getCurrentPlayerText() {
+    return element(by.css('.player-current')).getText();
+  }
+
+  getWinnerText() {
+    return element(by.css('.winner')).getText();
+  }
+
+  getDrawText() {
+    return element(by.css('.draw')).getText();
+  }
+
+  reset() {
+    return element(by.css('.reset')).click();
   }
 }
